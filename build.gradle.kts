1a6e2b575a12d544995be5f7177c3cacd550ac6c
Yaroslav Barashenko <y.barashenko@gmail.com>
2024-12-11 10:23:38 +0200

    Examples testing tools usage - Initial + first task

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.2")
+    testImplementation("io.mockk:mockk:1.13.7")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/CyclicShiftChecker.kt
@@ -0,0 +1,18 @@
+package org.example
+
+/*
+Припустимо, що існує метод isSubstring, який перевіряє, чи є
+одне слово підрядком іншого. Для двох рядків s1 і s2 напишіть код, який перевіряє
+рий перевіряє, чи отримано рядок s2 циклічним зсувом s1, використовуючи
+тільки один виклик методу isSubstring (приклад: слово waterbottle отримано
+циклічним зсувом erbottlewat).
+*/
+class CyclicShiftChecker
+{
+	fun isCyclicShiftOf(candidate: String, initial: String): Boolean
+	{
+		if (candidate.isBlank() || initial.isBlank() || candidate.length != initial.length) return false
+		return "$candidate$candidate".contains(initial)
+	}
+}
+
