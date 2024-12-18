5d09bbeec08f61ba4121cd99e7fa8a83b9b38453
Yaroslav Barashenko <y.barashenko@gmail.com>
2024-12-18 18:10:45 +0200

    Corrections
    (cherry picked from commit a76d166ed5fc9d647e722cd554d412f8b0ac16f7)

--- src/test/kotlin/practise/HelperTest.kt
+++ src/test/kotlin/practise/HelperTest.kt
@@ -1,8 +1,9 @@
+package practise
+
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.*
-import java.util.*
 
-class AdvancedParameterizedTests
+class HelperTest
 /*
 * Як правило, варто об'єднувати позитивні та негативні тестові сценарії в одному методі тільки тоді,
 * коли за вхідними параметрами можна легко визначити, який сценарій чому відповідає.
@@ -11,15 +12,6 @@ class AdvancedParameterizedTests
 * У такому разі кожен негативний і позитивний тестовий сценарій краще описати окремим тестом.
 */
 {
-	fun isValidPassword(password: String): Boolean
-	{
-		val lengthRequirement = password.length >= 8
-		val hasUppercase = password.any { it.isUpperCase() }
-		val hasLowercase = password.any { it.isLowerCase() }
-		val hasDigit = password.any { it.isDigit() }
-		val hasSpecialChar = password.any { "!@#$%^&*()_+".contains(it) }
-		return lengthRequirement && hasUppercase && hasLowercase && hasDigit && hasSpecialChar
-	}
 
 	@ParameterizedTest
 	@CsvSource(
@@ -35,52 +27,9 @@ class AdvancedParameterizedTests
 		assert(isValidPassword(password) == expected)
 	}
 
-
 	//todo yb додати приклади з CSVFileSource, MethodSource, EnumSource, ArgumentSource
 
 
-	fun isValidIpAddress(ip: String): Boolean
-	{
-		val regex = Regex("^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\\.){3}(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})$")
-		return regex.matches(ip)
-	}
-
-	@ParameterizedTest
-	@ValueSource(
-		strings = [
-			"192.168.1.1",
-			"255.255.255.255",
-			"0.0.0.0",
-			"127.0.0.1"
-		]
-	            )
-	fun `test isValidIpAddress with ValueSource (valid IPs)`(ip: String)
-	{
-		assert(isValidIpAddress(ip))
-	}
-
-	@ParameterizedTest
-	@EmptySource
-	@ValueSource(
-		strings = [
-			"256.256.256.256",
-			"123.456.789.0",
-			"192.168.1",
-			"192.168.1.1.1",
-			"abc.def.ghi.jkl"
-		]
-	            )
-	fun `test isValidIpAddress with ValueSource (invalid IPs)`(ip: String)
-	{
-		assert(!isValidIpAddress(ip))
-	}
-
-	fun areAnagrams(original: String, candidate: String): Boolean
-	{
-		return original.lowercase(Locale.getDefault()).toCharArray()
-			.sorted() == candidate.lowercase(Locale.getDefault())
-			.toCharArray().sorted()
-	}
 
 	@ParameterizedTest
 	@CsvSource(
