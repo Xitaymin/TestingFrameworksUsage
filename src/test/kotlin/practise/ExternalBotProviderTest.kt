167eca2935152138f51e8d61c8b14a7d9a4c2099
Yaroslav Barashenko <y.barashenko@gmail.com>
2024-12-18 17:30:31 +0200

    Cleaned practise task 1

--- src/test/kotlin/practise/ExternalBotProviderTest.kt
+++ src/test/kotlin/practise/ExternalBotProviderTest.kt
@@ -13,8 +13,8 @@ class ExternalBotProviderTest
 		assertTrue(user.name.length > 1, "Ім'я має бути довшим за 1 символ")
 		assertTrue(user.name.matches(Regex("[а-яА-Я]+")), "Ім'я має містити лише символи російської абетки")
 		assertTrue(user.email.endsWith("@yandex.ru") || user.email.endsWith("@mail.ru"), "Email має бути поштовою скринькою на Яндексі або Мейлі")
-		assertTrue(user.age in 18..60, "Вік має бути в діапазоні від 45 до 100")
-		assertTrue(user.aggressionPercentage > 85, "Треба бути агресивним, щоб не виділятися")
-		assertTrue(user.iq < 90, "Серед слухачів Соловйова немає геніїв")
+		assertTrue(user.age in 50..100, "Вік має бути в діапазоні від 50 до 100")
+		assertTrue(user.aggressionPercentage > 90, "Треба бути агресивним, щоб не виділятися, відсоток агресії має бути більше 90")
+		assertTrue(user.iq < 85, "Серед слухачів соловйова немає геніїв, IQ має бути менше 85")
 	}
 }
