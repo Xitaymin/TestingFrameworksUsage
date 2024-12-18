51b7fa3a5b50d4eeea75dab766ab4158d05e5b7e
Yaroslav Barashenko <y.barashenko@gmail.com>
2024-12-18 14:16:28 +0200

    Fixed example

--- src/main/kotlin/ValidationUtils.kt
+++ src/main/kotlin/ValidationUtils.kt
@@ -1,22 +1,61 @@
-class ValidationUtils {
-	open class ValidationException(message: String) : RuntimeException(message)
+/*
+* assertThrow
+* exception message
+* throw exception
+* окремий набір констант - Q
+* assertThrowsExactly
+* assertDoesn'tThrow
+* приклад з тестами з попередньої роботи
+*/
 
-	class DangerousException(message: String) : ValidationException(message)
 
-	companion object {
-		fun validateAge(age: Int) {
 
-			dangerousFunction()
 
-			when {
-				age < 0 -> throw ValidationException("Age cannot be negative")
-				age > 120 -> throw ValidationException("Age exceeds the realistic limit")
-			}
-		}
 
-		private fun dangerousFunction()
-		{
-			throw DangerousException("Dangerous check")
+open class ValidationException(message: String) : RuntimeException(message)
+
+private const val AGE_CANNOT_BE_NEGATIVE = "Age cannot be negative"
+private const val AGE_TO_BIG = "People don't live that long"
+
+//todo yb 4 - test the function
+fun validateAge(age: Int) {
+
+		dangerousFunction()
+
+		when {
+			age < 0 -> throw ValidationException(AGE_CANNOT_BE_NEGATIVE)
+			age > 120 -> throw ValidationException(AGE_TO_BIG)
 		}
 	}
-}
+
+
+	private fun dangerousFunction()
+	{
+//			will be implemented soon, doesn't do anything now
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	class DangerousException(message: String) : ValidationException(message)
+
