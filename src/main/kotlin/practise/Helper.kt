c88ce22ea2a77070f98c46b9f1bf2aeb09ca12ac
Yaroslav Barashenko <y.barashenko@gmail.com>
2024-12-18 18:36:00 +0200

    Task 2

--- src/main/kotlin/practise/Helper.kt
+++ src/main/kotlin/practise/Helper.kt
@@ -2,8 +2,13 @@ package practise
 
 import java.util.*
 
-fun isValidPassword(password: String): Boolean
+/*Валідний пароль має бути не коротшим 8 символів, містити літери обох регістрів, число та спеціальний символ з набору
+!@#$%^&*()_+
+*/
+
+fun isValidPassword(password: String?): Boolean
 {
+    if (password == null) return false
     val lengthRequirement = password.length >= 8
     val hasUppercase = password.any { it.isUpperCase() }
     val hasLowercase = password.any { it.isLowerCase() }
@@ -12,9 +17,32 @@ fun isValidPassword(password: String): Boolean
     return lengthRequirement && hasUppercase && hasLowercase && hasDigit && hasSpecialChar
 }
 
+/*Перевірити чи дві строки є анаграмами одна одної.
+Анаграми - це слова, які складаються з однакового набору літер, але можуть бути переставлені.
+Наприклад: listen та silent, Triangle та Integral
+ */
 fun areAnagrams(original: String, candidate: String): Boolean
 {
     return original.lowercase(Locale.getDefault()).toCharArray()
         .sorted() == candidate.lowercase(Locale.getDefault())
         .toCharArray().sorted()
 }
+
+
+/*
+* Функція для перевірки чи строка є паліндромом.
+* Паліндром - це слово, фраза, число або інша послідовність символів, яка однаково читається як зліва направо, так і справа наліво.
+* Наприклад: "madam", "racecar", "level", "А роза упала на лапу Азора"
+*/
+fun isPalindrome(candidate: String): Boolean{
+    for (i in 0 until candidate.length / 2)
+    {
+        if (candidate[i] != candidate[candidate.length - i - 1])
+        {
+            return false
+        }
+    }
+    return true
+}
+
+
